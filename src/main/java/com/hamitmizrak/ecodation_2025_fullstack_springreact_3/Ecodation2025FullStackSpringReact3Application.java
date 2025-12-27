dd56359ea2f2b6b3a57b380b683b4021701bee90
Hamit Mızrak <hamitmizrak@gmail.com>
2025-12-27 10:06:07 +0300

    Spring boot, react Initial commit

---
+++ src/main/java/com/hamitmizrak/ecodation_2025_fullstack_springreact_3/Ecodation2025FullStackSpringReact3Application.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak.ecodation_2025_fullstack_springreact_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecodation2025FullStackSpringReact3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecodation2025FullStackSpringReact3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamitmizrak/ecodation_2025_fullstack_springreact_3/Ecodation2025FullStackSpringReact3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak.ecodation_2025_fullstack_springreact_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ecodation2025FullStackSpringReact3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
