e6216657288c897f98aabf9980d63cda2ea41709
Hamit Mızrak <hamitmizrak@gmail.com>
2025-12-27 10:10:46 +0300

    revize

--- src/main/java/com/hamitmizrak/ecodation_2025_fullstack_springreact_3/Ecodation2025FullStackSpringReact3Application.java
+++ src/main/java/com/hamitmizrak/ecodation_2025_fullstack_springreact_3/Ecodation2025FullStackSpringReact3Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Ecodation2025FullStackSpringReact3Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ecodation2025FullStackSpringReact3Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Ecodation2025FullStackSpringReact3Application.class, args);
+    }
 
 }
