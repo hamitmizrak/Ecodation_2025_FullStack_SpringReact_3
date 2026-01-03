9ed882b94e4221d31b176998cb936b0142897af4
Hamit Mızrak <hamitmizrak@gmail.com>
2026-01-03 09:54:15 +0300

    BlogCategoryDto BlogCategoryEntity IBlogCategoryRepository BlogCategoryMapper IBlogCategoryServices BlogCategoryServicesImpl IBlogCategoryApi BlogCategoryApiImpl

--- src/main/java/com/hamitmizrak/controller/api/impl/BlogCategoryApiImpl.java
+++ src/main/java/com/hamitmizrak/controller/api/impl/BlogCategoryApiImpl.java
@@ -5,28 +5,44 @@ import com.hamitmizrak.business.services.interfaces.IBlogCategoryServices;
 import com.hamitmizrak.controller.api.interfaces.IBlogCategoryApi;
 import com.hamitmizrak.data.entity.BlogCategoryEntity;
 import com.hamitmizrak.error.ApiResult;
+import com.hamitmizrak.utily.FrontEnd;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// LOMBOK
+@RequiredArgsConstructor
+@Log4j2
+
+// API
+@RestController
+@RequestMapping("/blog/category/api/v1.0.0")
+@CrossOrigin(origins = FrontEnd.REACT_URL)
 public class BlogCategoryApiImpl implements IBlogCategoryApi<BlogCategoryDto> {
 
     // Injection
     private final IBlogCategoryServices<BlogCategoryDto, BlogCategoryEntity> iBlogCategoryServices;
 
 
-    /// ALL DELETE-SPEED//////////////////////////////////////////////////////////////
-    /// CREATE
+    /// ALL SPEED- DELETE//////////////////////////////////////////////////////////////
+
+    // http://localhost:4444//blog/category/api/v1.0.0/speed-data/12
     @Override
+    @PostMapping("/speed-data/{count}")
+    public ResponseEntity<String> categoryApiSpeedData(@PathVariable("count") Integer data) {
+        return ResponseEntity.ok(iBlogCategoryServices.categorySpeedData(data==null ? 0 :data));
+    }
+
+
+    // http://localhost:4444//blog/category/api/v1.0.0/delete/all
+    @Override
+    @DeleteMapping("/delete/all")
     public ResponseEntity<String> categoryApiAllDetelete() {
-        return null;
+        return ResponseEntity.ok(iBlogCategoryServices.categoryDeleteAll());
     }
-
-    @Override
-    public ResponseEntity<String> categoryApiSpeedData(Integer data) {
-        return null;
-    }
-
 
     /// CRUD //////////////////////////////////////////////////////////////////////
     /// CREATE
--- src/main/java/com/hamitmizrak/controller/api/interfaces/IBlogCategoryApi.java
+++ src/main/java/com/hamitmizrak/controller/api/interfaces/IBlogCategoryApi.java
@@ -5,10 +5,10 @@ import org.springframework.http.ResponseEntity;
 
 public interface IBlogCategoryApi<D> extends ICrudApi<D> {
 
-    // ALL DELETE
-    public ResponseEntity<String> categoryApiAllDetelete();
-
     // SPEED DATA
     public ResponseEntity<String> categoryApiSpeedData(Integer data);
+
+    // ALL DELETE
+    public ResponseEntity<String> categoryApiAllDetelete();
 
 }
