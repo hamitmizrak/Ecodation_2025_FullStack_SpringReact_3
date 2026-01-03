3b51de681e75fc653033e2aef5e2543925726166
Hamit Mızrak <hamitmizrak@gmail.com>
2026-01-03 09:38:59 +0300

    BlogCategoryDto BlogCategoryEntity IBlogCategoryRepository BlogCategoryMapper IBlogCategoryServices BlogCategoryServicesImpl IBlogCategoryApi BlogCategoryApiImpl

--- src/main/java/com/hamitmizrak/controller/api/impl/BlogCategoryApiImpl.java
+++ src/main/java/com/hamitmizrak/controller/api/impl/BlogCategoryApiImpl.java
@@ -1,4 +1,57 @@
 package com.hamitmizrak.controller.api.impl;
 
-public class BlogCategoryApiImpl {
-}
+import com.hamitmizrak.business.dto.BlogCategoryDto;
+import com.hamitmizrak.business.services.interfaces.IBlogCategoryServices;
+import com.hamitmizrak.controller.api.interfaces.IBlogCategoryApi;
+import com.hamitmizrak.data.entity.BlogCategoryEntity;
+import com.hamitmizrak.error.ApiResult;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public class BlogCategoryApiImpl implements IBlogCategoryApi<BlogCategoryDto> {
+
+    // Injection
+    private final IBlogCategoryServices<BlogCategoryDto, BlogCategoryEntity> iBlogCategoryServices;
+
+
+    /// ALL DELETE-SPEED//////////////////////////////////////////////////////////////
+    /// CREATE
+    @Override
+    public ResponseEntity<String> categoryApiAllDetelete() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<String> categoryApiSpeedData(Integer data) {
+        return null;
+    }
+
+
+    /// CRUD //////////////////////////////////////////////////////////////////////
+    /// CREATE
+    @Override
+    public ResponseEntity<ApiResult<?>> objectApiCreate(BlogCategoryDto blogCategoryDto) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<ApiResult<List<BlogCategoryDto>>> objectApiList() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<ApiResult<?>> objectApiFindById(Long id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<ApiResult<?>> objectApiUpdate(Long id, BlogCategoryDto blogCategoryDto) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<ApiResult<?>> objectApiDelete(Long id) {
+        return null;
+    }
+} //end BlogCategoryApiImpl
